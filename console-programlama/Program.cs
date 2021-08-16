80190fdaa7094122989441b8bef8df2e11145582
Ulas <gs_dogan05@hotmail.com>
2021-08-16 22:17:08 +0300

    First Commit!

---
+++ console-programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace main_method
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Yaşınızı Giriniz");
+            string age = Console.ReadLine();
+            Console.WriteLine(name + ", " + age);
+
+            return 0;
+        }
+    }
+}
