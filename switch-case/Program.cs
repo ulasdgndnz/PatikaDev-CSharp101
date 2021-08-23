c69b785b33855c76a25360dff984d9cedad1bda8
Ulas <gs_dogan05@hotmail.com>
2021-08-23 10:56:56 +0300

    switch-case added

---
+++ switch-case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int second = DateTime.Now.Second;
+
+            switch (second)
+            {
+                case < 10:
+                    Console.WriteLine("Saniye 1-10 arasında");
+                    break;
+                
+                case < 20:
+                    Console.WriteLine("Saniye 11-20 arasında");
+                    break;
+                
+                case < 30:
+                    Console.WriteLine("Saniye 21-30 arasında");
+                    break;
+                case < 40:
+                    Console.WriteLine("Saniye 31-40 arasında");
+                    break;
+                case < 50:
+                    Console.WriteLine("Saniye 41-50 arasında");
+                    break;
+                case <= 60:
+                    Console.WriteLine("Saniye 51-60 arasında");
+                    break;
+
+                default:
+                    Console.WriteLine("Saniye 1-10 arasında");
+                    break;
+            }
+        }
+    }
+}
