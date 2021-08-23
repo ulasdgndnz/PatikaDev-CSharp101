dca1e80441fcd500edfa83b5c3355f86843807fa
Ulas <gs_dogan05@hotmail.com>
2021-08-23 10:04:46 +0300

    if-else-if added

---
+++ if-else-if/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = DateTime.Now.Hour;
+
+            string  HaberVer(double saat) => saat < 12 ? "12 den sonra" : "12 den önce";
+
+            Console.WriteLine(HaberVer(time) + " " + time);
+        }
+    }
+}
