89030e275d951c76c1091daa731df4f33f5e3e83
Ulas <gs_dogan05@hotmail.com>
2021-08-26 11:41:32 +0300

    while-foreach added

---
+++ while-foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, toplam;
+            string cevap;
+            while (true)
+            {
+                Console.WriteLine("Toplamak istediğiniz 3 sayıyı giriniz");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                z = int.Parse(Console.ReadLine());
+                toplam = x + y + z;
+                
+                Console.WriteLine("Toplamı: " + toplam);
+                Console.WriteLine("çıkmak için q yazın");
+                cevap = Console.ReadLine();
+                if (cevap == "q")
+                {
+                    break;
+                }
+            }
+            
+            Console.WriteLine("Harflerini görmek istediğiniz kelimeyi girin");
+            string harfler = Console.ReadLine();
+            foreach (var harf in harfler)
+            {
+                Console.WriteLine(harf);
+            }
+            
+        }
+    }
+}
